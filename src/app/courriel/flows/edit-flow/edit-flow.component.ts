4912603c9743eded9e2c5b1c4de74accb60c9a22
Loharano <mianalaloharano@gmail.com>
2021-07-26 20:27:08 +0300

    removed environment files

--- src/app/courriel/flows/edit-flow/edit-flow.component.ts
+++ src/app/courriel/flows/edit-flow/edit-flow.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap, map } from 'rxjs/operators';
+import { AppFile } from 'src/app/classes/file';
+import { Link } from 'src/app/classes/link';
+import { Strings } from 'src/app/classes/strings';
+import { FlowService } from '../flow.service';
 
 @Component({
   selector: 'app-edit-flow',
   templateUrl: './edit-flow.component.html',
-  styleUrls: ['./edit-flow.component.scss']
+  styleUrls: ['./edit-flow.component.scss'],
 })
 export class EditFlowComponent implements OnInit {
+  activeFile: AppFile | null = null;
+  flow_id = 0;
+  app_page = false;
+  loading = true;
+  Strings = Strings;
+  Link = Link;
 
-  constructor() { }
+  flow$ = this.route.queryParams.pipe(
+    switchMap((routeData: { flow_id: string }) => {
+      const flow_id = parseInt(routeData.flow_id);
+      return this.flowService
+        .getFlow(flow_id)
+        .pipe(map((flows: any[]) => flows[0]));
+    })
+  );
+  constructor(
+    private flowService: FlowService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
